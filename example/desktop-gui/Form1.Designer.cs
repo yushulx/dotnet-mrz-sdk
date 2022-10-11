d6d3e7eb2b271ba6eb469fb559599c21978009cf
yushulx <lingxiao1002@gmail.com>
2022-10-11 16:40:26 +0800

    Add menu item

--- example/desktop-gui/Form1.Designer.cs
+++ example/desktop-gui/Form1.Designer.cs
@@ -34,17 +34,21 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.enterLicenseKeyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.menuStrip2.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Location = new System.Drawing.Point(6, 11);
+            this.pictureBox1.Location = new System.Drawing.Point(6, 28);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(600, 800);
+            this.pictureBox1.Size = new System.Drawing.Size(600, 783);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -54,7 +58,7 @@
             this.button1.AllowDrop = true;
             this.button1.AutoEllipsis = true;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button1.Location = new System.Drawing.Point(1218, 11);
+            this.button1.Location = new System.Drawing.Point(1218, 29);
             this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(280, 74);
@@ -68,7 +72,7 @@
             this.button2.AllowDrop = true;
             this.button2.AutoEllipsis = true;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button2.Location = new System.Drawing.Point(1218, 93);
+            this.button2.Location = new System.Drawing.Point(1218, 116);
             this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(280, 74);
@@ -76,23 +80,22 @@
             this.button2.Text = "Camera Scan";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
-            
             // 
             // pictureBox2
             // 
             this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox2.Location = new System.Drawing.Point(613, 11);
+            this.pictureBox2.Location = new System.Drawing.Point(613, 28);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(600, 800);
+            this.pictureBox2.Size = new System.Drawing.Size(600, 783);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 7;
             this.pictureBox2.TabStop = false;
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(1218, 200);
+            this.richTextBox1.Location = new System.Drawing.Point(1218, 240);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(280, 610);
+            this.richTextBox1.Size = new System.Drawing.Size(280, 570);
             this.richTextBox1.TabIndex = 8;
             this.richTextBox1.Tag = "";
             this.richTextBox1.Text = "";
@@ -100,11 +103,36 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(1218, 180);
+            this.label1.Location = new System.Drawing.Point(1218, 212);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(128, 15);
+            this.label1.Size = new System.Drawing.Size(98, 15);
             this.label1.TabIndex = 9;
             this.label1.Text = "MRZ Information";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 24);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1507, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.enterLicenseKeyToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(1507, 24);
+            this.menuStrip2.TabIndex = 11;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // enterLicenseKeyToolStripMenuItem
+            // 
+            this.enterLicenseKeyToolStripMenuItem.Name = "enterLicenseKeyToolStripMenuItem";
+            this.enterLicenseKeyToolStripMenuItem.Size = new System.Drawing.Size(110, 20);
+            this.enterLicenseKeyToolStripMenuItem.Text = "Enter License Key";
+            this.enterLicenseKeyToolStripMenuItem.Click += new System.EventHandler(this.enterLicenseKeyToolStripMenuItem_Click);
             // 
             // Form1
             // 
@@ -118,12 +146,17 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.MainMenuStrip = this.menuStrip1;
             this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = ".NET MRZ Scanner";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -137,6 +170,9 @@
         private PictureBox pictureBox2;
         private RichTextBox richTextBox1;
         private Label label1;
+        private MenuStrip menuStrip1;
+        private MenuStrip menuStrip2;
+        private ToolStripMenuItem enterLicenseKeyToolStripMenuItem;
     }
 }
 
--- example/desktop-gui/Form1.cs
+++ example/desktop-gui/Form1.cs
@@ -8,6 +8,7 @@ using static Dynamsoft.MrzScanner;
 using System.Text.Json.Nodes;
 using System;
 using System.Windows.Forms;
+using System.Drawing;
 
 namespace Test
 {
@@ -24,17 +25,8 @@ namespace Test
         {
             InitializeComponent();
             FormClosing += new FormClosingEventHandler(Form1_Closing);
-            string license = "";
-            try
-            {
-                license = System.IO.File.ReadAllText(@"license.txt");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.ToString());
-                license = "DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==";
-            }
-            
+            string license = "DLS2eyJoYW5kc2hha2VDb2RlIjoiMjAwMDAxLTE2NDk4Mjk3OTI2MzUiLCJvcmdhbml6YXRpb25JRCI6IjIwMDAwMSIsInNlc3Npb25QYXNzd29yZCI6IndTcGR6Vm05WDJrcEQ5YUoifQ==";
+
             int ret = MrzScanner.InitLicense(license); // Get a license key from https://www.dynamsoft.com/customer/license/trialLicense?product=ddn
             if (ret != 0) MessageBox.Show("License is invalid!");
             scanner = MrzScanner.Create();
@@ -84,7 +76,7 @@ namespace Test
             Bitmap bitmap = BitmapConverter.ToBitmap(mat);
             return bitmap;
         }
-        
+
         private void button1_Click(object sender, EventArgs e)
         {
             StopScan();
@@ -123,26 +115,31 @@ namespace Test
             {
                 StartScan();
             }
-            else {
+            else
+            {
                 StopScan();
             }
         }
 
-        private void StartScan() {
+        private void StartScan()
+        {
             button2.Text = "Stop";
             isCapturing = true;
             thread = new Thread(new ThreadStart(FrameCallback));
             thread.Start();
         }
 
-        private void StopScan() {
+        private void StopScan()
+        {
             button2.Text = "Camera Scan";
             isCapturing = false;
             if (thread != null) thread.Join();
         }
 
-        private void FrameCallback() {
-            while (isCapturing) {
+        private void FrameCallback()
+        {
+            while (isCapturing)
+            {
                 capture.Read(_mat);
                 Mat copy = new Mat(_mat.Rows, _mat.Cols, MatType.CV_8UC3);
                 _mat.CopyTo(copy);
@@ -155,5 +152,45 @@ namespace Test
             StopScan();
         }
 
+        private void enterLicenseKeyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string license = InputBox("Enter License Key", "", "");
+            if (license != null && license != "")
+            {
+                int ret = MrzScanner.InitLicense(license);
+            }
+        }
+
+        public static string InputBox(string title, string promptText, string value)
+        {
+            Form form = new Form();
+            TextBox textBox = new TextBox();
+            Button buttonOk = new Button();
+            Button buttonCancel = new Button();
+
+            form.Text = title;
+            textBox.Text = value;
+
+            buttonOk.Text = "OK";
+            buttonCancel.Text = "Cancel";
+            buttonOk.DialogResult = DialogResult.OK;
+            buttonCancel.DialogResult = DialogResult.Cancel;
+
+            textBox.SetBounds(12, 36, 372, 20);
+            buttonOk.SetBounds(60, 72, 80, 30);
+            buttonCancel.SetBounds(260, 72, 80, 30);
+
+            form.ClientSize = new System.Drawing.Size(400, 120);
+            form.Controls.AddRange(new Control[] { textBox, buttonOk, buttonCancel });
+            form.FormBorderStyle = FormBorderStyle.FixedDialog;
+            form.StartPosition = FormStartPosition.CenterScreen;
+            form.MinimizeBox = false;
+            form.MaximizeBox = false;
+            form.AcceptButton = buttonOk;
+            form.CancelButton = buttonCancel;
+
+            DialogResult dialogResult = form.ShowDialog();
+            return textBox.Text;
+        }
     }
 }
