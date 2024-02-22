3231849c83d31d081a6d051c468e0204d9d7a8fd
yushulx <lingxiao1002@gmail.com>
2024-02-22 14:45:20 +0800

    first commit

---
+++ ios/sdk/MrzParser.cs
@@ -0,0 +1,119 @@
+﻿namespace Dynamsoft;
+
+using System;
+using System.Text.RegularExpressions;
+
+public static class MrzParser
+{
+    private static readonly Regex TypeRegex = new Regex(@"[I|P|V]");
+    private static readonly Regex NumericRegex = new Regex(@"[0-9]");
+    private static readonly Regex AlphaRegex = new Regex(@"[A-Za-z]");
+    private static readonly Regex GenderRegex = new Regex(@"[M|F|x|<]");
+
+    private static string ExtractAndClean(string source, int start, int length)
+    {
+        if (length < 0) return source;
+        return source.Substring(start, length).Replace('<', ' ').Trim();
+    }
+
+    private static string ExtractDate(string source, int start)
+    {
+        string dateString = source.Substring(start, 6);
+        if (AlphaRegex.IsMatch(dateString)) return "N/A";
+
+        int year = int.Parse(dateString.Substring(0, 2));
+        int month = int.Parse(dateString.Substring(2, 2));
+        int day = int.Parse(dateString.Substring(4, 2));
+
+        // Adjust the year
+        year += year > DateTime.Now.Year % 100 ? 1900 : 2000;
+
+        try
+        {
+            return new DateTime(year, month, day).ToString("yyyy/MM/dd");
+        }
+        catch
+        {
+            return "N/A";
+        }
+    }
+
+    private static string ExtracExpiration(string source, int start)
+    {
+        string dateString = source.Substring(start, 6);
+        if (AlphaRegex.IsMatch(dateString)) return "N/A";
+
+        int year = int.Parse(dateString.Substring(0, 2));
+        int month = int.Parse(dateString.Substring(2, 2));
+        int day = int.Parse(dateString.Substring(4, 2));
+
+        // Adjust the year
+        year += year >= 60 ? 1900 : 2000;
+
+        try
+        {
+            return new DateTime(year, month, day).ToString("yyyy/MM/dd");
+        }
+        catch
+        {
+            return "N/A";
+        }
+    }
+
+    public static MrzResult Parse(string[] lines)
+    {
+        if (lines.Length == 2) return ParseTwoLines(lines[0], lines[1]);
+        if (lines.Length == 3) return ParseThreeLines(lines[0], lines[1], lines[2]);
+        return new MrzResult();
+    }
+
+    public static MrzResult ParseTwoLines(string line1, string line2)
+    {
+        MrzResult mrzInfo = new MrzResult();
+
+        //if (!TypeRegex.IsMatch(line1[0].ToString())) return mrzInfo;
+
+        switch (line1[0])
+        {
+            case 'P':
+                mrzInfo.Type = "PASSPORT (TD-3)";
+                break;
+            case 'V':
+                mrzInfo.Type = line1.Length == 44 ? "VISA (MRV-A)" : "VISA (MRV-B)";
+                break;
+            case 'I':
+                mrzInfo.Type = "ID CARD (TD-2)";
+                break;
+        }
+
+        mrzInfo.Nationality = ExtractAndClean(line1, 2, 3);
+        mrzInfo.Surname = ExtractAndClean(line1, 5, line1.IndexOf("<<") - 5);
+        mrzInfo.GivenName = ExtractAndClean(line1, mrzInfo.Surname.Length + 7, line1.Length - mrzInfo.Surname.Length - 7);
+        mrzInfo.PassportNumber = ExtractAndClean(line2, 0, 9);
+        mrzInfo.IssuingCountry = ExtractAndClean(line2, 10, 3);
+        mrzInfo.BirthDate = ExtractDate(line2, 13);
+        mrzInfo.Gender = GenderRegex.IsMatch(line2[20].ToString()) ? line2[20].ToString().Replace('<', 'X') : "N/A";
+        mrzInfo.Expiration = ExtracExpiration(line2, 21);
+        mrzInfo.Lines = $"{line1}\n{line2}";
+        return mrzInfo;
+    }
+
+    public static MrzResult ParseThreeLines(string line1, string line2, string line3)
+    {
+        MrzResult mrzInfo = new MrzResult();
+
+        //if (!TypeRegex.IsMatch(line1[0].ToString())) return mrzInfo;
+        mrzInfo.Type = "ID CARD (TD-1)";
+
+        mrzInfo.Nationality = ExtractAndClean(line2, 15, 3);
+        mrzInfo.Surname = ExtractAndClean(line3, 0, line3.IndexOf("<<"));
+        mrzInfo.GivenName = ExtractAndClean(line3, mrzInfo.Surname.Length + 2, line3.Length - mrzInfo.Surname.Length - 2);
+        mrzInfo.PassportNumber = ExtractAndClean(line1, 5, 9);
+        mrzInfo.IssuingCountry = ExtractAndClean(line1, 2, 3);
+        mrzInfo.BirthDate = ExtractDate(line2, 0);
+        mrzInfo.Gender = GenderRegex.IsMatch(line2[7].ToString()) ? line2[7].ToString().Replace('<', 'X') : "N/A";
+        mrzInfo.Expiration = ExtracExpiration(line2, 8);
+        mrzInfo.Lines = $"{line1}\n{line2}\n{line3}";
+        return mrzInfo;
+    }
+}
---
+++ ios/sdk/MrzScanner.cs
@@ -0,0 +1,164 @@
+﻿using Com.Dynamsoft.Core;
+using Com.Dynamsoft.Dlr;
+
+namespace Dynamsoft
+{
+    public class MrzScanner
+    {
+        private MRZRecognizer? recognizer;
+        public class Result
+        {
+            public int Confidence { get; set; }
+            public string Text { get; set; }
+            public int[] Points { get; set; }
+
+            public Result()
+            {
+                Points = new int[8];
+                Text = "";
+            }
+        }
+
+        public enum ImagePixelFormat
+        {
+            IPF_BINARY,
+
+            /**0:White, 1:Black */
+            IPF_BINARYINVERTED,
+
+            /**8bit gray */
+            IPF_GRAYSCALED,
+
+            /**NV21 */
+            IPF_NV21,
+
+            /**16bit with RGB channel order stored in memory from high to low address*/
+            IPF_RGB_565,
+
+            /**16bit with RGB channel order stored in memory from high to low address*/
+            IPF_RGB_555,
+
+            /**24bit with RGB channel order stored in memory from high to low address*/
+            IPF_RGB_888,
+
+            /**32bit with ARGB channel order stored in memory from high to low address*/
+            IPF_ARGB_8888,
+
+            /**48bit with RGB channel order stored in memory from high to low address*/
+            IPF_RGB_161616,
+
+            /**64bit with ARGB channel order stored in memory from high to low address*/
+            IPF_ARGB_16161616,
+
+            /**32bit with ABGR channel order stored in memory from high to low address*/
+            IPF_ABGR_8888,
+
+            /**64bit with ABGR channel order stored in memory from high to low address*/
+            IPF_ABGR_16161616,
+
+            /**24bit with BGR channel order stored in memory from high to low address*/
+            IPF_BGR_888
+        }
+
+        public class LicenseVerificationListener : Java.Lang.Object, ILicenseVerificationListener
+        {
+            public void LicenseVerificationCallback(bool isSuccess, CoreException ex)
+            {
+                if (!isSuccess)
+                {
+                    throw new Exception(ex.ToString());
+                }
+            }
+        }
+
+        public static void InitLicense(string license, object? context = null)
+        {
+            if (context == null) { return; }
+
+            LicenseManager.InitLicense(license, (Android.Content.Context)context, new LicenseVerificationListener());
+        }
+
+        private MrzScanner()
+        {
+            recognizer = new MRZRecognizer();
+        }
+
+        public static MrzScanner Create()
+        {
+            return new MrzScanner();
+        }
+
+        ~MrzScanner()
+        {
+            Destroy();
+        }
+
+        public void Destroy()
+        {
+            recognizer = null;
+        }
+
+        public static string? GetVersionInfo()
+        {
+            return MRZRecognizer.Version;
+        }
+
+        public Result[]? DetectFile(string filename)
+        {
+            if (recognizer == null) return null;
+
+            DLRResult[]? mrzResult = recognizer.RecognizeFile(filename);
+            return GetResults(mrzResult);
+        }
+
+        public Result[]? DetectBuffer(byte[] buffer, int width, int height, int stride, ImagePixelFormat format)
+        {
+            if (recognizer == null) return null;
+
+            ImageData imageData = new ImageData()
+            {
+                Bytes = buffer,
+                Width = width,
+                Height = height,
+                Stride = stride,
+                Format = (int)format,
+            };
+            DLRResult[]? mrzResult = recognizer.RecognizeBuffer(imageData);
+            return GetResults(mrzResult);
+        }
+
+        private Result[]? GetResults(DLRResult[]? mrzResult)
+        {
+            if (mrzResult != null && mrzResult[0].LineResults != null)
+            {
+                DLRLineResult[] lines = mrzResult[0].LineResults.ToArray();
+                Result[] result = new Result[lines.Length];
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    result[i] = new Result()
+                    {
+                        Confidence = lines[i].Confidence,
+                        Text = lines[i].Text ?? "",
+                        Points = new int[8]
+                        {
+                            lines[i].Location.Points[0].X,
+                            lines[i].Location.Points[0].Y,
+                            lines[i].Location.Points[1].X,
+                            lines[i].Location.Points[1].Y,
+                            lines[i].Location.Points[2].X,
+                            lines[i].Location.Points[2].Y,
+                            lines[i].Location.Points[3].X,
+                            lines[i].Location.Points[3].Y
+                        }
+                    };
+                }
+
+                return result;
+            }
+
+            return null;
+        }
+    }
+}
+
+
